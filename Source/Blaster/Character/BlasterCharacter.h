aeda6784c2c19282d933740e9da638029e0c14cf
invi1998 <2267661826@qq.com>
2022-10-07 00:27:28 +0800

    睡觉

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -25,6 +25,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
 private:
 	// Ìí¼Óµ¯»É±ÛºÍÉãÏñ»ú
 
