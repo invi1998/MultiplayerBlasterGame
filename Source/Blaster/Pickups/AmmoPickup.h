5db4f74e9e65220a804f15e56be2ec027793574c
invi1998 <2267661826@qq.com>
2024-01-16 21:46:22 +0800

    AmmoPickup

---
+++ Source/Blaster/Pickups/AmmoPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "AmmoPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AAmmoPickup : public APickup
+{
+	GENERATED_BODY()
+	
+};
