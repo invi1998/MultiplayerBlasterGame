d4feaf3ea9b56ec5d6b5c33e692337d494f755b1
invi1998 <2267661826@qq.com>
2024-01-21 10:54:56 +0800

    血宝拾取蓝图，添加拾取音效，销毁特效

---
+++ Source/Blaster/Pickups/HealthPickup.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+	virtual void Destroyed() override;
+
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                             const FHitResult& SweepResult) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float HealthAmount = 100.0f;	// 血量
+
+	UPROPERTY(EditAnywhere)
+	float HealingTime = 5.0f;	// 治疗时间，因为我们希望在拾取后有一个治疗的过程，而不是立刻就回满血量，所以我们需要一个治疗时间
+
+	UPROPERTY(VisibleAnywhere)
+	class UNiagaraComponent* PickupEffectNiagaraComponent;		// 拾取特效
+
+	UPROPERTY(EditAnywhere)
+	class UNiagaraSystem* PickupEffectNiagaraSystem;		// 拾取特效的NiagaraSystem
+};
