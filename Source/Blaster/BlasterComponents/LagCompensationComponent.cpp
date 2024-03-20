278e40991703950f22cb59bc6e21c83af705d349
invi1998 <2267661826@qq.com>
2024-03-20 23:01:03 +0800

    处理玩家延迟的组件（服务器倒带）

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+
+// Called when the game starts
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LagCompensationComponent.generated.h"
+
+
+// 处理玩家延迟的组件（服务器倒带）
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BLASTER_API ULagCompensationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	ULagCompensationComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+};
